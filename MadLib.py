239df0aa57c416020baecd2595399ff157e96da7
AsmirAWS <67970766+AsmirAWS@users.noreply.github.com>
2020-07-27 22:20:53 -0400

    MadLib in Python

---
+++ MadLib.py
@@ -0,0 +1,7 @@
+name = input("Enter a teacher's name: ")
+subject = input("Enter name of subject " + name + " teaches: ")
+gender = input("Is the teacher a he or a she?: ")
+characteristic = input("Enter something positive about the teacher: ")
+
+print(name + " teaches " + subject)
+print(gender + " is "+characteristic)
